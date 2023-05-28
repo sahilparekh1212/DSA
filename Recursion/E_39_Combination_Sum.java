cf71cf29fa48ef20967673d486b6953e51fd3f69
= <sahiparekh1212@gmail.com>
2023-05-27 21:40:42 -0400

    Added 7 Recursion Questions

---
+++ Recursion/E_39_Combination_Sum.java
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/combination-sum/solutions/16502/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partitioning/
+
+class Solution {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(ans, new ArrayList<>(), candidates, target, 0);
+        // helper2(ans, candidates, target, 0, new ArrayList(), 0);
+        return ans;
+    }
+
+    private void helper(List<List<Integer>> ans, List<Integer> tempAns, int[] candidates, int targetRemaining,
+            int startIndex) {
+        if (targetRemaining < 0) {
+            return;
+        }
+        if (targetRemaining == 0) {
+            ans.add(new ArrayList<>(tempAns));
+            return;
+        }
+        for (int i = startIndex; i < candidates.length; i++) {
+            tempAns.add(candidates[i]);
+            helper(ans, tempAns, candidates, targetRemaining - candidates[i], i);
+            tempAns.remove(tempAns.size() - 1);
+        }
+    }
+
+    private void helper2(List<List<Integer>> ans, int[] candidates, int target, int i, List<Integer> tempAns,
+            int tempTotal) {
+        if (tempTotal > target || (i >= candidates.length)) {
+            return;
+        }
+        if (tempTotal == target) {
+            ans.add(new ArrayList<>(tempAns));
+            return;
+        }
+        tempAns.add(candidates[i]);
+        helper2(ans, candidates, target, i, tempAns, tempTotal + candidates[i]);
+        tempAns.remove(tempAns.size() - 1);
+        helper2(ans, candidates, target, i + 1, tempAns, tempTotal);
+    }
+
+}
---
+++ Recursion/E_90_Subsets_II.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList();
+        // helper(nums,0,new ArrayList(),ans);
+        helper2(ans, new ArrayList<>(), nums, 0);
+        return ans;
+    }
+
+    private void helper(int[] nums, int startIndex, List<Integer> tempAnsList, List<List<Integer>> ans) {
+        if (startIndex >= nums.length) {
+            ans.add(new ArrayList(tempAnsList));
+            return;
+        }
+
+        // Here if we were not allowed to have any duplicate fo any number in
+        // tempAnsList, we would have removed duplicates from the subsetsWithDup()
+
+        // Inlude the nums[startIndex] in the tempAnsList
+        tempAnsList.add(nums[startIndex]);
+        // As we cannot include duplicates of the self, we are using index startIndex+1
+        helper(nums, startIndex + 1, tempAnsList, ans);
+
+        // skipping through duplicates from right as ans must not have duplicates of
+        // nums[startIndex]
+        while (startIndex < nums.length - 1 && nums[startIndex] == nums[startIndex + 1]) {
+            startIndex += 1;
+        }
+
+        // do not the nums[startIndex] in the ans
+        tempAnsList.remove(tempAnsList.size() - 1);
+        helper(nums, startIndex + 1, tempAnsList, ans);
+    }
+
+    private void helper2(List<List<Integer>> ans, List<Integer> tempAns, int[] nums, int startIndex) {
+        ans.add(new ArrayList<>(tempAns));
+        for (int i = startIndex; i < nums.length; i++) {
+            if (i > startIndex && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            tempAns.add(nums[i]);
+            helper2(ans, tempAns, nums, i + 1);
+            tempAns.remove(tempAns.size() - 1);
+        }
+    }
+
+}
