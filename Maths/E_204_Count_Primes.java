521e6b94ecc4425e7afe0d998269f0a647e9f694
= <sahiparekh1212@gmail.com>
2023-06-11 22:10:04 -0400

    Added 2 maths question

---
+++ Maths/E_204_Count_Primes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] primesArr = new boolean[n + 1];
+
+        Arrays.fill(primesArr, true);
+        int i = 2;
+        while (i <= n / 2) {
+            int ref = i + i;
+            while (ref <= n) {
+                if (primesArr[ref]) {
+                    primesArr[ref] = false;
+                }
+                ref += i;
+            }
+            i++;
+        }
+        int ans = 0;
+        for (int j = 2; j < n; j++) {
+            if (primesArr[j] == true) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
