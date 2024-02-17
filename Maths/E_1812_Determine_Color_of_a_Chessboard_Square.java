65dffcae60ac35b46e87620670601d6b9d0ce963
= <sahiparekh1212@gmail.com>
2024-02-17 15:43:18 -0500

    Cleanup

--- Maths/E_1812_Determine_Color_of_a_Chessboard_Square.java
+++
@@ -1,5 +0,0 @@
-class Solution {
-    public boolean squareIsWhite(String c) {
-        return ((c.charAt(0) - 'a' + c.charAt(1) - '1') % 2 != 0);
-    }
-}
--- Maths/E_268_Missing_Number.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int missingNumber(int[] nums) {
-        int sum = nums.length * (nums.length + 1) / 2;
-        for (int i : nums) {
-            sum -= i;
-        }
-        return sum;
-    }
-}
